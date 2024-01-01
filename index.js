032ad3164c9490b5608c248739445ab79ea35e51
VoTrieuAn <voan2068@gmail.com>
2024-01-01 23:48:57 +0700

    Connect database

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const routesClient = require("./routes/client/index.route.js");
 const dotenv = require("dotenv");
-
+//Khởi tạo dotenv
 dotenv.config();
+//Kết thúc khởi tạo
+//Kết nối cơ sở dữ liệu
+const database = require("./config/database.js");
+database.connect();
+//Kết thúc kết nối cơ sở dữ liệu
 const app = express();
 //const port = 3000; //Tránh để lộ vì bảo mật
 const port = process.env.PORT;
+//Nhúng mongoose vào
 
 //Thiết lập xử lý các view
 app.set("views", "./views");
