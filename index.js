ea15181c9c15fcf2962ccb6726fe5a8267346965
VoTrieuAn <voan2068@gmail.com>
2024-07-07 21:03:47 +0700

    Nhúng file tĩnh

--- index.js
+++ index.js
@@ -13,6 +13,24 @@ const port = process.env.PORT;
 //End access enviroment variable in env
 app.set('views', './views');
 app.set('view engine', 'pug');
+
+//Create static file
+/**
+ * Dùng để lưu trữa các tài nguyên tĩnh như:
+ *  + Hình ảnh
+ *  + CSS
+ *  + JavaScript
+ *  + Font chữ
+ *  + Các tệp tin khác mà trình duyệt wetb có thể yêu cầu khi người dùng truy cập vào web
+ * ==> Hiệu quả:
+ *  + Giúp load nhanh hơn so với việc xử lý qua framework hay server. Cải thiện thời gian phản hồi
+ *  + Cache: giảm lượng dữ liệu phải tải lại
+ *  + Tổ chức dễ dàng
+ *  + Bảo mật: Tập tin như mã nguồn server-side và các dữ liệu nhạy cảm không được phép truy cập từ client-side
+ */
+app.use(express.static('public'));
+//End create static file
+
 //Dip routes
 routesClient(app);
 
