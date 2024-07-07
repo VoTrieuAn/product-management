54419b661af2d7c6a03aa86d673eeb7a4bf227b4
VoTrieuAn <voan2068@gmail.com>
2024-07-07 17:44:16 +0700

    Cấu hình .env

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const routesClient = require('./routes/client/index.route.js');
+const dotenv = require('dotenv');
+
+//Load environment variable
+dotenv.config();
+//End enviroment variable
 
 const app = express();
-const port = 3000;
 
+// Access enviroment variable in env
+const port = process.env.PORT;
+//End access enviroment variable in env
 app.set('views', './views');
 app.set('view engine', 'pug');
 //Dip routes
