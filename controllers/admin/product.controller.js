1f724eea0cabeae2cdb4a2e43cc51334df02903c
VoTrieuAn <voan2068@gmail.com>
2024-07-08 17:42:54 +0700

    Tính năng tìm kiếm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,7 @@
 //[GET] /admin/products/
 const Product = require("../../models/product.model");
 module.exports.index = async (req, res) => {
+
   const filterState = [
     {
       name: 'Tất cả',
@@ -19,6 +20,7 @@ module.exports.index = async (req, res) => {
     }
   ];
 
+
   if(req.query.status) {
     //Tìm và trả về vị trí của phẩn tử
     const index = filterState.findIndex(item => item.status == req.query.status);
@@ -32,15 +34,24 @@ module.exports.index = async (req, res) => {
   }
   //req thông điệp của người dùng gửi cho máy chủ
   //res thông của máy chủ trả về cho phía người dùng
+  //req.query trả ra một object key=value sau dấu ?
   if(req.query.status) { 
     find.status = req.query.status;
   }
+
+  //Search
+  if(req.query.keyword) {
+    const regex = new RegExp(req.query.keyword, "i");
+    find.title = regex;
+  }
+  //End search
 
   const products = await Product.find(find);
 
   res.render('./admin/pages/products/index.pug', {
       pageTitle: 'Danh sách sản phẩm',
       products: products,
-      filterState: filterState
+      filterState: filterState,
+      keyword: req.query.keyword //Trả ra cho value có giá trị mặc định là keyword
   });
 }
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -2,7 +2,6 @@
 const buttons = document.querySelectorAll("[button-status]");
 if(buttons.length > 0) {
   let url = new URL(location.href); //Khởi tạo một url
-  console.log(location.href);
   buttons.forEach((button) => {
     button.addEventListener('click', () => {
       const status = button.getAttribute('button-status');
@@ -18,3 +17,24 @@ if(buttons.length > 0) {
   });
 }
 //End button status
+
+//Fix submit multiple conditions
+const formSearch = document.querySelector('#form-search');
+console.log(formSearch);
+if(formSearch) {
+  //Mặc định của form là load lại trang
+  formSearch.addEventListener('submit', (event) => {
+    event.preventDefault(); //Ngăn chặn hành vi mặc định
+    let url = new URL(location.href); //Khởi tạo một url
+    const keyword = event.target.keyword.value
+    if(keyword) {
+      //Dùng để update lại đường link tham số (key, value) thêm ?key=value
+      url.searchParams.set("keyword", keyword); 
+    } else {
+      url.searchParams.delete("keyword"); //Dùng để xóa đi key trên url sau dấu ?
+    }
+
+    location.href = url.href; //Set lại url cho trang web
+  });
+}
+//End fix submit multiple conditions
