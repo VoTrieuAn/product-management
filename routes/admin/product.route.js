c19297916f2a5a5f24e0ae75c1d2d1ef011fd8d1
VoTrieuAn <voan2068@gmail.com>
2024-07-17 00:29:35 +0700

    Đẩy file tĩnh lên clound

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,45 +1,119 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const controllerProduct= require('../../controllers/admin/product.controller');
-const multer  = require('multer');
-const storageMulter = require('../../helpers/storage-multer.helper');
-const upload = multer({ storage: storageMulter(multer) });
-const validate = require('../../validates/admin/product.validate');
+const controllerProduct = require("../../controllers/admin/product.controller");
+const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const streamifier = require("streamifier");
+const dotenv = require('dotenv');
+// const storageMulter = require('../../helpers/storage-multer.helper');
+// const upload = multer({ storage: storageMulter(multer) });
+const upload = multer();
+const validate = require("../../validates/admin/product.validate");
+
+// Configuration cloudinary
+// dotenv phải được gọi tại nơi mình sử dụng
+dotenv.config();
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_KEY,
+  api_secret: process.env.CLOUD_SECRET,
+});
+// End configuration cloudinary
 
 //[GET] /admin/products
-router.get('/', controllerProduct.index);
+router.get("/", controllerProduct.index);
 
 //[PATCH] /admin/products/change-status/:status/:_id
-router.patch('/change-status/:status/:_id', controllerProduct.changeState);
+router.patch("/change-status/:status/:_id", controllerProduct.changeState);
 
 //[PATCH] /admin/products/change-multi
-router.patch('/change-multi', controllerProduct.changeMulti);
+router.patch("/change-multi", controllerProduct.changeMulti);
 
 //[DELETE] /admin/products/delete/:_id
-router.delete('/delete/:_id', controllerProduct.deleteItem);
+router.delete("/delete/:_id", controllerProduct.deleteItem);
 
 //[GET] /admin/products/create
-router.get('/create', controllerProduct.create);
+router.get("/create", controllerProduct.create);
 
 //[POST] /admin/products/create
 router.post(
-  '/create', 
-  upload.single('thumbnail'),
+  "/create",
+  upload.single("thumbnail"),
+  function (req, res, next) {
+    if (req.file) {
+      let streamUpload = (req) => {
+        return new Promise((resolve, reject) => {
+          let stream = cloudinary.uploader.upload_stream((error, result) => {
+            if (result) {
+              resolve(result);
+            } else {
+              reject(error);
+            }
+          });
+
+          streamifier.createReadStream(req.file.buffer).pipe(stream);
+        });
+      };
+
+      async function upload(req) {
+        let result = await streamUpload(req);
+        console.log(result.url);
+        // Add new line code
+        const key = req.file.fieldname; // Get key name image
+        req.body[key] = result.url;
+        // Run next step
+        next(); // next qua controller
+      }
+      upload(req);
+    } else {
+      next();
+    }
+  },
   validate.createPost,
   controllerProduct.createPost
 );
 
 // [GET] /admin/proudcts/edit/:_id
-router.get('/edit/:_id', controllerProduct.edit);
+router.get("/edit/:_id", controllerProduct.edit);
 
 // [PATCH] /admin/proudcts/edit/:_id
 router.patch(
-  '/edit/:_id',
-  upload.single('thumbnail'),
+  "/edit/:_id",
+  upload.single("thumbnail"),
+  function (req, res, next) {
+    if (req.file) {
+      let streamUpload = (req) => {
+        return new Promise((resolve, reject) => {
+          let stream = cloudinary.uploader.upload_stream((error, result) => {
+            if (result) {
+              resolve(result);
+            } else {
+              reject(error);
+            }
+          });
+
+          streamifier.createReadStream(req.file.buffer).pipe(stream);
+        });
+      };
+
+      async function upload(req) {
+        let result = await streamUpload(req);
+        console.log(result.url);
+        // Add new line code
+        const key = req.file.fieldname; // Get key name image
+        req.body[key] = result.url;
+        // Run next step
+        next(); // next qua controller
+      }
+      upload(req);
+    } else {
+      next();
+    }
+  },
   validate.createPost,
   controllerProduct.editPatch
-)
+);
 
 // [GET] /admin/products/detail/:_id
-router.get('/detail/:_id', controllerProduct.detail)
+router.get("/detail/:_id", controllerProduct.detail);
 module.exports = router;
