0c87a5b912f9fe21a8398f93e54ae56863523f98
VoTrieuAn <voan2068@gmail.com>
2024-07-23 17:48:57 +0700

    Tính năng chỉnh sửa danh mục sản phẩm

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -27,7 +27,6 @@ module.exports.create = async (req, res) => {
 
 //[POST] /admin/products-category/create
 module.exports.createPost = async (req, res) => {
-
   const position = req.body.position
   if(position == '') {
     const countProduct = await ProductCategory.countDocuments();
@@ -44,4 +43,53 @@ module.exports.createPost = async (req, res) => {
   req.flash('success', 'Thêm mới danh mục sản phẩm thành công!');
 
   res.redirect(`/${systemConfig.prefixAdmin}/products-category`);
+}
+
+// [GET] /admin/products-category/edit/:_id
+module.exports.edit = async (req, res) => {
+  try {
+    const data = await ProductCategory.findOne({
+      _id: req.params._id,
+      deleted: false
+    });
+
+  
+    const records = await ProductCategory.find({
+      deleted: false,
+    });
+  
+    const newRecords = createTreeHelper(records);
+  
+    res.render('admin/pages/products-category/edit', {
+      pageTitle: 'Chỉnh sửa danh mục sản phẩm',
+      productCategory: newRecords,
+      data: data
+    });
+  } catch (error) {
+    res.redirect(`/${systemConfig.prefixAdmin}/products-category`);
+  }
+  
+}
+// [PATCH] /admin/products-category/edit/:_id
+module.exports.editPatch = async (req, res) => {
+  try {
+    const position = req.body.position;
+    if(position == '') {
+      const countProduct = await ProductCategory.countDocuments();
+      req.body.position = countProduct + 1;
+    } else {
+      req.body.position = parseInt(position);
+    }
+
+    await ProductCategory.updateOne({
+      _id: req.params._id,
+      deleted: false
+    }, req.body);
+  
+    req.flash('success', 'Cập nhật danh mục sản phẩm thành công!');
+
+    res.redirect('back');
+  } catch (error) {
+    res.redirect(`/${systemConfig.prefixAdmin}/products-category`);
+  }
 }
--- routes/admin/product-category.route.js
+++ routes/admin/product-category.route.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const controllerProductCategory = require("../../controllers/admin/product-category.controller");
+const controller = require("../../controllers/admin/product-category.controller");
 const multer = require("multer");
 const uploadCloud = require("../../middlewares/admin/upload-cloud.middleware");
 const upload = multer();
 const validate = require("../../validates/admin/product-category.validate");
 
 // [GET] /admin/products-category
-router.get("/", controllerProductCategory.index);
+router.get("/", controller.index);
 // [GET] /admin/products-category/create
-router.get("/create", controllerProductCategory.create);
+router.get("/create", controller.create);
 
 //[POST] /admin/products-category/create
 router.post(
@@ -17,7 +17,19 @@ router.post(
   upload.single("thumbnail"),
   uploadCloud.uploadSingle,
   validate.createPost,
-  controllerProductCategory.createPost
+  controller.createPost
+);
+
+// [GET] /admin/products-category/edit/:_id
+router.get("/edit/:_id", controller.edit)
+
+// [PATCH] /admin/products-category/edit/:_id
+router.patch(
+  "/edit/:_id",
+  upload.single("thumbnail"),
+  uploadCloud.uploadSingle,
+  validate.createPost,
+  controller.editPatch
 );
 
 module.exports = router;
