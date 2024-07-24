db8d13866962dbb97279eeafb7c5802282d76bc8
VoTrieuAn <voan2068@gmail.com>
2024-07-24 17:36:21 +0700

    Xây dựng phần Thêm mới nhóm quyền

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -1,6 +1,28 @@
+const Role = require("../../models/role.model");
+const systemConfig = require('../../config/system');
 //[GET] /admin/roles
-module.exports.index = (req, res) => {
-  res.render('./admin/pages/roles/index.pug', {
-      pageTitle: 'Nhóm quyền'
+module.exports.index = async (req, res) => {
+  records = await Role.find({
+    deleted: false
   });
+
+  res.render('./admin/pages/roles/index.pug', {
+      pageTitle: 'Nhóm quyền',
+      roles: records
+  });
+}
+
+//[GET] /admin/roles/create
+module.exports.create = (req, res) => {
+  res.render('./admin/pages/roles/create.pug', {
+      pageTitle: 'Thêm mới nhóm quyền'
+  });
+}
+//[POST] /admin/roles/create
+module.exports.createPost = async (req, res) => {
+  const record  = new Role(req.body);
+
+  await record.save();
+  
+  res.redirect(`/${systemConfig.prefixAdmin}/roles`);
 }
--- routes/admin/role.route.js
+++ routes/admin/role.route.js
@@ -5,4 +5,10 @@ const controller = require('../../controllers/admin/role.controller');
 //[GET] /admin/roles
 router.get('/', controller.index);
 
+//[GET] /admin/roles/create
+router.get('/create', controller.create);
+
+//[POST] /admin/roles/create
+router.post('/create', controller.createPost);
+
 module.exports = router;
