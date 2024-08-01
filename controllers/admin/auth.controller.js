9f67dcbe23c68d0fee0def12b35b1a145293ff1e
VoTrieuAn <voan2068@gmail.com>
2024-08-01 15:23:58 +0700

    Làm tính năng đăng xuất

--- controllers/admin/auth.controller.js
+++ controllers/admin/auth.controller.js
@@ -1,13 +1,13 @@
 const md5 = require("md5");
 const Account = require("../../models/account.model");
 const systemConfig = require('../../config/system');
-//[GET] /admin/auth
+//[GET] /admin/auth/login
 module.exports.login = (req, res) => {
   res.render("admin/pages/auth/login", {
     pageTitle: "Đăng nhập",
   })
 }
-//[POST] /admin/auth
+//[POST] /admin/auth/login
 module.exports.loginPost = async (req, res) => {
   const email = req.body.email;
   const password = md5(req.body.password);
@@ -32,4 +32,13 @@ module.exports.loginPost = async (req, res) => {
   res.cookie("token", user.token);
 
   res.redirect(`/${systemConfig.prefixAdmin}/dashboard`);
+}
+
+//[GET] /admin/auth/logout
+module.exports.logout = (req, res) => {
+  // Ghi thế này thì dùng để áp dụng cho tất cả các trang
+  // Đọc chi tiết hơn tại: https://expressjs.com/en/5x/api.html#res.cookie
+  res.clearCookie('token');
+
+  res.redirect(`/${systemConfig.prefixAdmin}/auth/login`);
 }
--- routes/admin/auth.route.js
+++ routes/admin/auth.route.js
@@ -5,7 +5,10 @@ const controller = require('../../controllers/admin/auth.controller');
 //[GET] /admin/auth
 router.get('/login', controller.login);
 
-//[GET] /admin/auth
+//[POST] /admin/auth/login
 router.post('/login', controller.loginPost);
+
+//[GET] /admin/auth/logout
+router.get('/logout', controller.logout);
 
 module.exports = router;
