9b2636a5e92d258da42cb62502e0fe5396fac72b
VoTrieuAn <voan2068@gmail.com>
2024-08-18 20:56:06 +0700

    Làm trang Thông tin cá nhân bên admin (chỉnh sửa thông tin cá nhân)

--- controllers/admin/my-account.controller.js
+++ controllers/admin/my-account.controller.js
@@ -1,6 +1,36 @@
+const md5 = require('md5');
+const Account = require('../../models/account.model');
+
 //[GET] /admin/my-account
 module.exports.index = (req, res) => {
-  res.render('./admin/pages/my-account/index.pug', {
+  res.render('admin/pages/my-account/index.pug', {
       pageTitle: 'Trang thông tin cá nhân'
   });
+}
+
+//[GET] /admin/my-account/edit
+module.exports.edit = async (req, res) => {
+  res.render('admin/pages/my-account/edit.pug', {
+    pageTitle: 'Cập nhật thông tin cá nhân'
+  });
+}
+
+//[PATCH] /admin/my-account/edit
+module.exports.editPatch = async (req, res) => {
+  const id = res.locals.user.id;
+  try {
+    if(req.body.password) {
+      req.body.password = md5(req.body.password);
+    } else {
+      delete req.body.password;
+    }
+  
+    await Account.updateOne({
+      _id: id
+    }, req.body);
+    req.flash('success', 'Cập nhật thông tin thành công');
+  } catch (error) {
+    req.flash('error', 'Cập nhật thông tin thất bại');
+  }
+  res.redirect('back');
 }
--- routes/admin/my-account.route.js
+++ routes/admin/my-account.route.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../../controllers/admin/my-account.controller');
-
+const multer = require("multer");
+const uploadCloud = require("../../middlewares/admin/upload-cloud.middleware");
+const upload = multer();
 //[GET] /admin/my-account
 router.get('/', controller.index);
 
+router.get('/edit', controller.edit);
+
+router.patch(
+  '/edit',
+  upload.single("avatar"),
+  uploadCloud.uploadSingle,
+  controller.editPatch
+)
 module.exports = router;
