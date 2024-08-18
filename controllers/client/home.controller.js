a5dffa722850594ba3da1d662e6483e30a4c50ce
VoTrieuAn <voan2068@gmail.com>
2024-08-18 21:59:07 +0700

    Hiển thị menu chung bên client (Tái sử dụng cho các trang có header)

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,16 +1,7 @@
 //Nhiệm vụ chứa controller của route home
-const ProductCategory = require("../../models/product-category.model");
-const createTreeHelper = require('../../helpers/create-tree.helper');
 //[GET] /
 module.exports.index = async (req, res) => {
-  const categoryProducts = await ProductCategory.find({
-    deleted: false,
-  });
-  
-  const newCategoryProducts = createTreeHelper(categoryProducts);
-
   res.render('./client/pages/home/index.pug', {
     pageTitle: 'Trang chủ',
-    layoutCategoryProducts: newCategoryProducts
   });
 }
--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,10 +1,14 @@
 //Nhiệm vụ là chứa các route tổng
-
 const productRoutes = require('./product.route');
 const homeRoute = require('./home.route');
+const categoryMiddleware = require('../../middlewares/client/category.middleware');
 module.exports = (app) => { //Cách export trong node js
-    app.use('/', homeRoute);
-    app.use("/products", productRoutes);
+  // Khi viết ở đây các trang sẽ chạy qua middleware category trước vì category này không cần bảo mật => Đảm bảo các router đều chạy qua middleware này
+  app.use(categoryMiddleware.category);
+
+  app.use('/', homeRoute);
+
+  app.use("/products", productRoutes);
 }
 /**
  * Không nên ghi ra các tên phương thức cụ thể get, put, patch vì các con sẽ ăn theo
