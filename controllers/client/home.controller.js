c576f9aca1675c664e5c7a36a8e27510c22e5255
VoTrieuAn <voan2068@gmail.com>
2024-08-19 12:43:16 +0700

    Hiển thị danh sách sản phẩm nổi bật

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,7 +1,22 @@
+const Product = require('../../models/product.model');
+
 //Nhiệm vụ chứa controller của route home
 //[GET] /
 module.exports.index = async (req, res) => {
+  const productFeatured = await Product.find({
+    featured: "1",
+    status: "active",
+    deleted: false
+  }).sort({
+    position: "desc"
+  }).limit(6);
+
+  for(const item of productFeatured) {
+    item.priceNew = item.price * (100 - item.discountPercentage) / 100;
+  }
+
   res.render('./client/pages/home/index.pug', {
     pageTitle: 'Trang chủ',
+    productFeatured: productFeatured,
   });
 }
