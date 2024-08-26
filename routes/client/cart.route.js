c649c48828ee8591d96308ab09526c5b814022a2
VoTrieuAn <voan2068@gmail.com>
2024-08-26 14:13:32 +0700

    Làm trang giỏ hàng

--- routes/client/cart.route.js
+++ routes/client/cart.route.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../../controllers/client/cart.controller');
 
+// [POST] /cart/
+router.get('/', controller.index);
+
 // [POST] /cart/add/productId
 router.post('/add/:productId', controller.addPost);
 
