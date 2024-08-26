ec2f753ac31246e0da5b628274f1f9a6732ce5dd
VoTrieuAn <voan2068@gmail.com>
2024-08-26 13:32:52 +0700

    Làm nút Mini Cart trên Header

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -12,6 +12,13 @@ module.exports.cart = async (req, res, next) => {
       // expires là key để set thời gian hết hạn cho cookie
       expires: new Date(Date.now() + expire)
     });  
+  } else {
+    // Trường hợp load lại trang và đã có giỏ hàng
+    const cart = await Cart.findOne({
+      _id: req.cookies.cartId
+    });
+
+    res.locals.cart = cart;
   }
 
   next();
